4648914d0450f14011184a29eb385562065eedd4
Jimmm-lab <jz792424@gmail.com>
2025-06-19 15:42:37 +0800

    Create solution.py

---
+++ ITSA新手入門專區/01 基本指令使用/Problem 04.中英字典/solution.py
@@ -0,0 +1,15 @@
+animal = {
+    "dog": "狗",
+    "cat": "貓",
+    "duck": "鴨",
+    "cow": "牛",
+    "fox": "狐",
+    "狗": "dog",
+    "貓": "cat",
+    "鴨": "duck",
+    "牛": "cow",
+    "狐": "fox"
+}
+
+a = input()
+print(animal[a])
